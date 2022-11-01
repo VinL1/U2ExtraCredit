3963cdea717c071924c2bce352881914c65cf5a1
Lightr <vincentl105@nycstudents.net>
2022-11-01 18:26:22 -0400

    spaced equations

--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -4,28 +4,28 @@ public class CalculatorRunner {
     public static void main (String [] args) {
         Scanner input = new Scanner (System.in);
 
-        System.out.println ("Enter an expression with *, /, +, -, %, or ^: ");
+        System.out.print ("Enter an expression with *, /, +, -, %, or ^: ");
         String inp = input.nextLine();
 
         Calculator calc = new Calculator(inp);
 
         if (inp.contains ("*")) {
-            System.out.println(calc.multiply());
+            System.out.println("The answer is: " + calc.multiply());
         }
         else if (inp.contains("/")) {
-            System.out.println(calc.divide());
+            System.out.println("The answer is: " + calc.divide());
         }
         else if (inp.contains ("+")) {
-            System.out.println(calc.add());
+            System.out.println("The answer is: " + calc.add());
         }
         else if (inp.contains ("%")) {
-            System.out.println(calc.mod());
+            System.out.println("The answer is: " + calc.mod());
         }
         else if (inp.contains ("^")) {
-            System.out.println(calc.exponent());
+            System.out.println("The answer is: " + calc.exponent());
         }
         else {
-            System.out.println(calc.subtract());
+            System.out.println("The answer is: " + calc.subtract());
         }
     }
 }
